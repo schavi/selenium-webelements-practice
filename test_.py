0a92a4c03223dc98cf6d5d216db06b2e5748f7f1
kalla <aronschn@gmail.com>
2024-04-24 12:05:04 +0200

    new tests written

--- test_.py
+++ test_.py
@@ -1,18 +1,21 @@
 
 
-from selenium import webdriver
-from selenium.webdriver import ActionChains
-
 from page_model import TheInternetPage
 
-from time import sleep
+from selenium.webdriver import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
 
 import pytest
+
+from random import choice as randomchoice
+
 
 
 class TestTheInternetPageFirefox:
 
-    BASEURL = "https://the-internet.herokuapp.com/"
+    PROTOCOL ="https://"
+    BASEURL = "the-internet.herokuapp.com/"
 
     def setup_method(self):
         self.page = TheInternetPage(chosen_browser="firefox")
@@ -31,6 +34,9 @@ class TestTheInternetPageFirefox:
             print(link.text)
 
     
+    # A-B testing
+
+
     def test_add_remove(self):
         link_text = "Add/Remove Elements"
         self.page.main_links()[1][self.page.main_links()[0].index(link_text)].click()
@@ -43,6 +49,45 @@ class TestTheInternetPageFirefox:
         assert len(self.page.add_remove_delete_buttons()) == 1, f"Page should have 1 added element; has {len(self.page.add_remove_delete_buttons())}"
         self.page.add_remove_delete_buttons()[0].click()
         assert len(self.page.add_remove_delete_buttons()) == 0, "Added elements not removed"
+
+
+    @pytest.mark.parametrize("username, password, valid", [("admin", "admin", True),
+                                                           ("invalid", "invalid", False)
+                                                          ])
+    def test_basic_auth(self, username, password, valid):
+        self.page.browser.get(self.PROTOCOL + username + ":" + password + "@" + self.BASEURL + "/basic_auth")
+        if valid:
+            assert self.page.basic_auth_success_mesage() != None, "Success message wasn't found"
+        else:
+            self.page.browser.switch_to.alert.dismiss()     # invalid credentials make the authorization alert pop-up
+            assert "Not authorized" in self.page.browser.page_source, "Couldn't find 'Not authorized' message on page"
+
+
+    # Broken images
+
+
+    def test_challenging_dom_buttons(self):
+        link_text = "Challenging DOM"
+        self.page.main_links()[1][self.page.main_links()[0].index(link_text)].click()
+        blue_button_color = "rgb(43, 166, 203)"
+        red_button_color = "rgb(198, 15, 19)"
+        green_button_color = "rgb(93, 164, 35)"
+        assert self.page.challenging_dom_blue_button().value_of_css_property("background-color") == blue_button_color
+        assert self.page.challenging_dom_red_button().value_of_css_property("background-color") == red_button_color
+        assert self.page.challenging_dom_green_button().value_of_css_property("background-color") == green_button_color
+
+
+    def test_challenging_dom_table_contents(self):
+        link_text = "Challenging DOM"
+        self.page.main_links()[1][self.page.main_links()[0].index(link_text)].click()
+        for column_name in self.page.challenging_dom_table_header():
+            print(column_name, end=" ")
+        print("\n")
+        for row in self.page.challenging_dom_table_content():
+            for column in row:
+                print(column, end=" ")
+            print()
+        print("\n")
 
 
     def test_checkboxes(self):
@@ -67,6 +112,11 @@ class TestTheInternetPageFirefox:
             assert False, "Alert wasn't found"
 
 
+    # Digest auth
+
+    # Disappearing elements
+
+
     def test_drag_and_drop(self):
         link_text = "Drag and Drop"
         self.page.main_links()[1][self.page.main_links()[0].index(link_text)].click()
@@ -87,6 +137,75 @@ class TestTheInternetPageFirefox:
         assert self.page.dropdown_options()[2].get_attribute("selected") == "true", "Failed to select option 2"
 
 
-    def test_dynamic_controls(self):
-        pass
+    # Dynamic content
+
+    # Dynamic controls
+
+    # Dynamic loading
+
+    # Entry ad--TODO----NEXT--
+
+    # Exit intent
+
+
+    def test_file_download(self):
+        link_text = "File Download"
+        self.page.main_links()[1][self.page.main_links()[0].index(link_text)].click()
+        randomchoice(self.page.file_download_links()).click()
+        # TODO check result
+
+
+    # File upload--TODO--
+
+    # Floating Menu
+
+    # Forgot Password
+
+    # Form Authentication--TODO----NEXT--
+
+    # Frames--TODO----NEXT--
+
+    # Geolocation
+
+    # Horizontal Slider--TODO----NEXT--
+
+    # Hovers--TODO--
+
+    # Infinite Scroll--TODO--
+
+    # Inputs
+
+    # JQuery UI Menus--TODO--
+
+    # JavaScript Alerts--TODO--
+
+    # JavaScript onload event error
+
+    # Key Presses--TODO----NEXT--
+
+    # Large & Deep DOM
+
+    # Multiple Windows--TODO----NEXT--
+
+    # Nested Frames--TODO----NEXT--
+
+    # Notification Messages
+
+    # Redirect Link
+
+    # Secure File Download
+
+    # Shadow DOM--TODO--
+
+    # Shifting Content
+
+    # Slow Resources--TODO--
+
+    # Sortable Data Tables
+
+    # Status Codes
+
+    # Typos
+
+    # WYSIWYG Editor--TODO--
 
